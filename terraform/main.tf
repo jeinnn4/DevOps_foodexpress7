9fc2a327a1f7ad5abeb1201a6191c289a186ac04
jeinnn4 <somavateysorn@gmail.com>
2026-04-15 16:02:27 +0700

    updated ubuntu section in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,8 +67,16 @@ resource "aws_security_group" "foodexpress_sg" {
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"]   # Canonical (Ubuntu official)
-  filter { name = "name";                values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"] }
-  filter { name = "virtualization-type"; values = ["hvm"] }
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 # ── EC2 Instance ──────────────────────────────────────────────────
